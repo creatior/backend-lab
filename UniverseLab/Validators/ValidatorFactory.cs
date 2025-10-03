9620a663af5435d180ac33d62fc532b752053c07
creatior <vartem2020@mail.ru>
2025-10-03 20:16:37 +0300

    L1 p38 added validator factory

--- UniverseLab/Validators/ValidatorFactory.cs
+++ UniverseLab/Validators/ValidatorFactory.cs
@@ -1,6 +1,11 @@
+using FluentValidation;
+
 namespace universe_lab.Validators;
 
-public class ValidatorFactory
+public class ValidatorFactory(IServiceProvider serviceProvider)
 {
-    
+    public IValidator<T> GetValidator<T>()
+    {
+        return serviceProvider.GetRequiredService<IValidator<T>>()!;
+    }
 }
